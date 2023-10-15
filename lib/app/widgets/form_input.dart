bf13d5e6df1b37e8f08690fa2eade0eb08397604
kyhchn <kyaahh69@gmail.com>
2023-10-15 08:07:17 +0700

    `feat` add some cusomization

--- lib/app/widgets/form_input.dart
+++ lib/app/widgets/form_input.dart
@@ -3,22 +3,26 @@ import 'package:pretest_mdi_pai/app/data/themes/typography.dart';
 import 'package:sizer/sizer.dart';
 
 class FormInput extends StatefulWidget {
-  final String label;
+  final String? label;
   final String hint;
   final TextInputType inputType;
   final TextEditingController controller;
   final bool? readOnly;
   final String? Function(String?)? validator;
+  final Widget? suffixIcon;
+  final void Function(String)? onFieldSubmitted;
 
-  const FormInput({
-    Key? key,
-    required this.label,
-    required this.hint,
-    required this.inputType,
-    required this.controller,
-    this.validator,
-    this.readOnly,
-  }) : super(key: key);
+  const FormInput(
+      {Key? key,
+      this.label,
+      required this.hint,
+      required this.inputType,
+      required this.controller,
+      this.onFieldSubmitted,
+      this.validator,
+      this.readOnly,
+      this.suffixIcon})
+      : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
@@ -40,19 +44,22 @@ class _FormInputState extends State<FormInput> {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(width: 2.h),
-            Text(
-              widget.label,
-              style: TypographyStyles.b2.semibold(),
-            ),
-          ],
-        ),
+        widget.label != null
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(width: 2.h),
+                  Text(
+                    widget.label!,
+                    style: TypographyStyles.b2.semibold(),
+                  ),
+                ],
+              )
+            : const SizedBox(),
         SizedBox(height: 1.h),
         TextFormField(
           validator: super.widget.validator,
+          onFieldSubmitted: widget.onFieldSubmitted,
           controller: widget.controller,
           readOnly: widget.readOnly ?? false,
           keyboardType: widget.inputType,
@@ -61,22 +68,19 @@ class _FormInputState extends State<FormInput> {
               : false,
           style: TypographyStyles.b3.semibold(color: Colors.grey.shade600),
           decoration: InputDecoration(
-            prefixIcon: Icon(
-              Icons.person,
-              color: Colors.grey.shade600,
-            ),
             hintText: widget.hint,
-            suffixIcon: widget.inputType == TextInputType.visiblePassword
-                ? IconButton(
-                    icon: Icon(
-                      isPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                      color: Colors.grey.shade600,
-                    ),
-                    onPressed: togglePasswordVisibility,
-                  )
-                : null,
+            suffixIcon: widget.suffixIcon ??
+                (widget.inputType == TextInputType.visiblePassword
+                    ? IconButton(
+                        icon: Icon(
+                          isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.grey.shade600,
+                        ),
+                        onPressed: togglePasswordVisibility,
+                      )
+                    : null),
           ),
         ),
       ],
