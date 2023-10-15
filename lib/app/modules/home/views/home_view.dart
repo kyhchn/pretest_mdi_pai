ec8c914beb85278e1ab03a9f9e24e9ebab6fefa8
kyhchn <kyaahh69@gmail.com>
2023-10-15 08:07:53 +0700

    `feat` slice homepage

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:pretest_mdi_pai/app/widgets/form_input.dart';
+import 'package:sizer/sizer.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -10,15 +12,52 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        title: const Text('Pretest'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+          child: RefreshIndicator(
+        onRefresh: controller.fetch,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 1.h),
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: FormInput(
+                    hint: 'search',
+                    inputType: TextInputType.text,
+                    onFieldSubmitted: (value) => controller.fetch(),
+                    suffixIcon: IconButton(
+                        onPressed: controller.fetch,
+                        icon: const Icon(Icons.search)),
+                    controller: controller.nameEditingController),
+              ),
+              Expanded(
+                child: Obx(
+                  () => ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(controller.users[index].image),
+                          ),
+                          title: Text(controller.users[index].fullName),
+                          subtitle: Text(controller.users[index].email),
+                          trailing:
+                              Text(controller.users[index].age.toString()),
+                        ),
+                      );
+                    },
+                    itemCount: controller.users.length,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+      )),
     );
   }
 }
